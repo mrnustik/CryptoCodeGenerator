75dc64be75043f704f4d112786b13feb96ecb6ec
Michal Mrnuštík <michal@mrnustik.net>
2018-09-28 21:55:07 +0200

    Add project files.

---
+++ CryptoCodeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoCodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

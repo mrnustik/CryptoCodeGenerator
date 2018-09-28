26f8c819a1a13a236b4b4df8dc5101343cfe1b92
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-09-28 23:24:39 +0200

    Fix invalid variable naming in VariationsGeneratorTests

--- CryptoCodeGenerator.Tests/VariationsGeneratorTests.cs
+++ CryptoCodeGenerator.Tests/VariationsGeneratorTests.cs
@@ -6,35 +6,35 @@ namespace CryptoCodeGenerator.Tests
     public class VariationsGeneratorTests
     {
         [Fact]
-        public void GeneratePermutations_AlphabetOfTwoCharacters_CreatesAllPermutations()
+        public void GenerateVariations_AlphabetOfTwoCharacters_CreatesAllPermutations()
         {
             //Arrange
             var sut = CreateSUT();
             var alphabet = new[] { '0', '1' };
             //Act
-            var permutations = sut.GenerateVariations(alphabet, 2)
+            var variations = sut.GenerateVariations(alphabet, 2)
                 .ToList();
 
             //Assert
-            Assert.Contains(permutations, t => t == "00");
-            Assert.Contains(permutations, t => t == "01");
-            Assert.Contains(permutations, t => t == "10");
-            Assert.Contains(permutations, t => t == "11");
+            Assert.Contains(variations, t => t == "00");
+            Assert.Contains(variations, t => t == "01");
+            Assert.Contains(variations, t => t == "10");
+            Assert.Contains(variations, t => t == "11");
         }
 
         [Fact]
-        public void GeneratePermutations_AlphabetOfTwoCharactersWithLength1_CreatesAllPermutations()
+        public void GenerateVariations_AlphabetOfTwoCharactersWithLength1_CreatesAllPermutations()
         {
             //Arrange
             var sut = CreateSUT();
             var alphabet = new[] { '0', '1' };
             //Act
-            var permutations = sut.GenerateVariations(alphabet, 1)
+            var variations = sut.GenerateVariations(alphabet, 1)
                 .ToList();
 
             //Assert
-            Assert.Contains(permutations, t => t == "0");
-            Assert.Contains(permutations, t => t == "1");
+            Assert.Contains(variations, t => t == "0");
+            Assert.Contains(variations, t => t == "1");
         }
 
         private IVariationsGenerator CreateSUT()

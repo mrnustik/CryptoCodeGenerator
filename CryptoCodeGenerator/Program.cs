7f7562a90d1b41022ec439258dc6d540c45cb9c2
Michal Mrnustik <michal.mrnustik@outlook.com>
2024-10-18 19:27:59 +0200

    Fix typo in variable name

--- CryptoCodeGenerator/Program.cs
+++ CryptoCodeGenerator/Program.cs
@@ -9,8 +9,8 @@ namespace CryptoCodeGenerator
             var requirements = new CodeRequirements(4, 9, 3);
             var distanceCalculator = new HammingDistanceCalculator();
             var variationsGenerator = new VariationsGenerator();
-            var codeGeneraor = new CodeGenerator(variationsGenerator, distanceCalculator);
-            foreach (var generateCodeSpecifiedByRequirement in codeGeneraor.GenerateCodeSpecifiedByRequirements(requirements, new[] {'0', '1', '2'}))
+            var codeGenerator = new CodeGenerator(variationsGenerator, distanceCalculator);
+            foreach (var generateCodeSpecifiedByRequirement in codeGenerator.GenerateCodeSpecifiedByRequirements(requirements, new[] {'0', '1', '2'}))
             {
                 Console.WriteLine(generateCodeSpecifiedByRequirement);
             }
